3feaef2b35e71486e764cb040397a54639fd4b96
ArturT <arturtrzop@gmail.com>
2016-09-24 15:42:34 +0200

    invoke rake task for rspec

--- lib/knapsack_pro/runners/rspec_runner.rb
+++ lib/knapsack_pro/runners/rspec_runner.rb
@@ -3,13 +3,16 @@ module KnapsackPro
     class RSpecRunner < BaseRunner
       def self.run(args)
         ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
+        ENV['KNAPSACK_PRO_RECORDING_ENABLED'] = 'true'
 
         runner = new(KnapsackPro::Adapters::RSpecAdapter)
 
-        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec rspec #{args} --default-path #{runner.test_dir} -- #{runner.stringify_test_file_paths}]
-
-        Kernel.system(cmd)
-        Kernel.exit($?.exitstatus) unless $?.exitstatus.zero?
+        require 'rspec/core/rake_task'
+        RSpec::Core::RakeTask.new(:spec) do |t|
+          t.rspec_opts = "#{args} --default-path #{runner.test_dir}"
+          t.pattern = runner.test_file_paths
+        end
+        Rake::Task[:spec].invoke
       end
     end
   end
